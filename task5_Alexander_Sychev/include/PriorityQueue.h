833e1b8116e8a3f79dd787a62ffc367dfb9725a5
alexsychev27@gmail.com <alexsychev27@gmail.com>
2023-06-04 13:00:42 +0300

    Created a project

---
+++ task5_Alexander_Sychev/include/PriorityQueue.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <list>
+#include <exception>
+
+
+// any templates?
+class PriorityQueue {
+   
+public:
+	
+   // You need to complete the implement : 
+	//void push(const T& t); 
+	//T poll();
+	void push(const int t);
+	int poll();
+
+private:
+// add relevant data members
+};
